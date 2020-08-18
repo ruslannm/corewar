a501e0b5f146628edb06e14a1e7487a49b4d36b1
mukaseevru <github@mukaseev.ru>
2020-08-18 21:13:23 +0300

    +free in exit_func.c

--- sources/asm/exit_func.c
+++ sources/asm/exit_func.c
@@ -12,7 +12,74 @@
 
 #include "asm.h"
 
-void	terminate(t_parser *parser, const char *error_info, const char *func)
+static void	free_tokens(t_token **tokens)
+{
+	int		i;
+
+	if (tokens)
+	{
+		i = 0;
+		while (tokens[i])
+		{
+			if (tokens[i])
+			{
+				free(tokens[i]->content);
+				free(tokens[i]->op_tab);
+				free(tokens[i]);
+				tokens[i] = NULL;
+			}
+			i++;
+		}
+		free(tokens);
+	}
+	tokens = NULL;
+}
+
+static void	free_labels(t_label **labels)
+{
+	int		i;
+
+	if (labels)
+	{
+		i = 0;
+		while (labels[i])
+		{
+			if (labels[i])
+			{
+				free(labels[i]->content);
+				free(labels[i]);
+				labels[i] = NULL;
+			}
+			i++;
+		}
+		free(labels);
+	}
+	labels = NULL;
+}
+
+static void	free_links(t_link **links)
+{
+	int		i;
+
+	if (links)
+	{
+		i = 0;
+		while (links[i])
+		{
+			if (links[i])
+			{
+				free(links[i]);
+				links[i] = NULL;
+			}
+			i++;
+		}
+		free(links);
+	}
+	links = NULL;
+}
+
+void		terminate(t_parser *parser, const char *error_info,
+						const char *func)
 {
 	ft_putstr_fd("ERROR: ", 2);
 	ft_putstr_fd(error_info, 2);
@@ -21,10 +88,17 @@ void	terminate(t_parser *parser, const char *error_info, const char *func)
 	exit_func(parser, -1);
 }
 
-void	exit_func(t_parser *parser, int error)
+void		exit_func(t_parser *parser, int error)
 {
 	if (!parser)
 	{
+		free_tokens(parser->tokens);
+		free_labels(parser->labels);
+		free_links(parser->links);
+		free(parser->code);
+		free(parser->name);
+		free(parser->comment);
+		free(parser);
 		exit(error);
 	}
 	exit(error);
