246ac2b407af3d0e40ffa5e6b0c650f862fc72f1
Lharys Noisome <lnoisome@il-k3.msk.21-school.ru>
2020-08-26 21:38:12 +0300

    add1

--- includes/utils.h
+++ includes/utils.h
@@ -1,37 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.h                                            :+:      :+:    :+:   */
+/*   lidft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: lnoisome <lnoisome@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/31 20:10:42 by lnoisome          #+#    #+#             */
-/*   Updated: 2020/08/26 18:29:06 by rgero            ###   ########.fr       */
+/*   Updated: 2020/08/26 21:08:21 by lnoisome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
-    
+
 # include <unistd.h>
 # include <stdio.h>
 
-size_t				ft_strlen(const char *s);
-void				*ft_memcpy(void *dst, const void *src, size_t n);
-void *ft_calloc(size_t elem, size_t size_elem);
-void *ft_memset(void *ptr, int c, size_t bytes);
-int		ft_strequ(const char *s1, const char *s2);
+size_t		ft_strlen(const char *s);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_calloc(size_t elem, size_t size_elem);
+void		*ft_memset(void *ptr, int c, size_t bytes);
+int			ft_strequ(const char *s1, const char *s2);
 __int64_t	ft_atoi(const char *s);
-int	ft_strcmp(const char *s1, const char *s2);
-int ft_isdigit(char c);
-void ft_swap(t_player **a, t_player **b);
-void	ft_putnbr(int nb);
-void	ft_putstr(const char *s);
-void	ft_putchar(char c);
-void free_data(t_vm *vm);
-void	print_memory(const void *arena, size_t size);
-t_car *del_start(t_car **head, t_car *cur_car);
-t_car *del_end(t_car *cur_car);
-t_car *del_midl(t_car *cur_car);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_isdigit(char c);
+void		ft_swap(t_player **a, t_player **b);
+void		ft_putnbr(int nb);
+void		ft_putstr(const char *s);
+void		ft_putchar(char c);
+void		free_data(t_vm *vm);
+void		print_memory(const void *arena, size_t size);
+t_car		*del_start(t_car **head, t_car *cur_car);
+t_car		*del_end(t_car *cur_car);
+t_car		*del_midl(t_car *cur_car);
 
 #endif
--- includes/vm.h
+++ includes/vm.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vm.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: ccharmai <5429549@mail.ru>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/07 10:15:03 by lnoisome          #+#    #+#             */
-/*   Updated: 2020/08/26 18:28:12 by rgero            ###   ########.fr       */
+/*   Updated: 2020/08/26 20:12:34 by ccharmai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,64 +16,64 @@
 # include "op.h"
 # include "oper.h"
 
-typedef struct s_car
+typedef struct		s_car
 {
-	int id;
-	unsigned int step;  /*количество байт, которые нужно будет «перешагнуть», чтобы оказаться на следующей операции*/
-	int regist[REG_NUMBER];
-	struct s_car *next;
-	struct s_car *prev;
-	int last_live_cycle; /*цикл, в котором в последний раз была выполнена операция live*/
-	int cycle_to_action; /*количество циклов до исполнения операции*/
-	int code; /*код операции*/
-	int position; /*позиция каретки*/
-	int carry;
-}				t_car;
+	int				id;
+	unsigned int	step;
+	int				regist[REG_NUMBER];
+	struct s_car	*next;
+	struct s_car	*prev;
+	int				last_live_cycle;
+	int				cycle_to_action;
+	int				code;
+	int				position;
+	int				carry;
+}					t_car;
 
-typedef struct s_player
+typedef struct		s_player
 {
-	char *name;
-	char *ex_code;
-	size_t ex_size;
-	int flag_n;
-	int id;
-	char *comment;
-}				t_player;
+	char			*name;
+	char			*ex_code;
+	size_t			ex_size;
+	int				flag_n;
+	int				id;
+	char			*comment;
+}					t_player;
 
-typedef struct s_players
+typedef struct		s_players
 {
-	t_player	**arr;
-	int32_t dump_num;
-	int8_t  dump_size;
-	int size;
-	int iter;
-}		t_players;
+	t_player		**arr;
+	int32_t			dump_num;
+	int8_t			dump_size;
+	int				size;
+	int				iter;
+}					t_players;
 
-typedef struct s_vm
+typedef struct		s_vm
 {
 	unsigned char	arena[MEM_SIZE];
-	int cycle_to_die;
-	t_players	players;
-	int id_car;
-	int num_car;
-	t_car *carry;
-	int count_check; 
-	int count_live; /*Количество операций live*/
-	int iter; /* количество циклов, прошедших после старта*/
-	t_op  operations;
-	int win_id;
-	int cur_cycle;
-	int dump_value;
-	int dump_size;
-}						t_vm;
+	int				cycle_to_die;
+	t_players		players;
+	int				id_car;
+	int				num_car;
+	t_car			*carry;
+	int				count_check;
+	int				count_live;
+	int				iter;
+	t_op			operations;
+	int				win_id;
+	int				cur_cycle;
+	int				dump_value;
+	int				dump_size;
+}					t_vm;
 
-typedef struct s_pars
+typedef struct		s_pars
 {
-	char **av;
-	int ac;
-	int i;
-	int fd;
-	int     dump_flag;
-}				t_pars;
+	char			**av;
+	int				ac;
+	int				i;
+	int				fd;
+	int				dump_flag;
+}					t_pars;
 
 #endif
